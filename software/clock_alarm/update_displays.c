20cf8cbf4442337bbe7b3219b2422734d88d397c
Arturo Cordoba <arturocv16@gmail.com>
2021-04-30 01:22:25 -0600

    7 display conversion

--- software/clock_alarm/update_displays.c
+++ software/clock_alarm/update_displays.c
@@ -10,6 +10,34 @@ volatile unsigned char *SECOND_2_DATA = (unsigned char *) SEC_2_BASE;
 
 
 /**
+ * Converts an int into the 7 display equivalent
+**/
+int int_to_7display(int number) {
+    switch (number) {
+        case 0:  // 0000001
+            return 1;
+        case 1:  // 1001111
+            return 79;
+        case 2:  // 0010010
+            return 18;
+        case 3:  // 0000110
+            return 6;
+        case 4:  // 1001100
+            return 76;
+        case 5:  // 0100100
+            return 36;
+        case 6:  // 0100000
+            return 32;
+        case 7:  // 0001111
+            return 15;
+        case 8:  // 0000000
+            return 0;
+        default:  // 0000100
+            return 4;
+    }
+}
+
+/**
  * This function updates display's data based on the current time
 **/
 void update_displays(int current_time) {
@@ -24,12 +52,12 @@ void update_displays(int current_time) {
     // Get the seconds based on the remainder of the minutes calculation
     int seconds = seconds_left % 60;
 
-    *HOUR_1_DATA = hours / 10;  // First hours digit
-    *HOUR_2_DATA = hours % 10;  // Second hours digit
+    *HOUR_1_DATA = int_to_7display(hours / 10);  // First hours digit
+    *HOUR_2_DATA = int_to_7display(hours % 10);  // Second hours digit
 
-    *MINUTE_1_DATA = minutes / 10;  // First minutes digit
-    *MINUTE_2_DATA = minutes % 10;  // Second minutes digit
+    *MINUTE_1_DATA = int_to_7display(minutes / 10);  // First minutes digit
+    *MINUTE_2_DATA = int_to_7display(minutes % 10);  // Second minutes digit
 
-    *SECOND_1_DATA = seconds / 10;  // First seconds digit
-    *SECOND_2_DATA = seconds % 10;  // First seconds digit
+    *SECOND_1_DATA = int_to_7display(seconds / 10);  // First seconds digit
+    *SECOND_2_DATA = int_to_7display(seconds % 10);  // First seconds digit
 }
